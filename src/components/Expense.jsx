a469f834b71f3552b6efc23ac1f41ee0a09bd417
shegz101 <bobatesegun3@gmail.com>
2023-04-11 23:59:57 +0100

    calculate-balance-function-created

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Transaction from "./Transaction";
 import TransactionOverview from "./TransactionOverview";
@@ -14,6 +14,27 @@ const Wrapper = styled.div`
 
 const Expense = () => {
   const [transacts, PopulateTransacts] = useState([]);
+  const [expsenses, setExpenses] = useState(0);
+  const [incomes, setIncomes] = useState(0);
+
+  //function to calculate the balance
+  const calculateBalance = () => {
+    //first set the expense and income to zero
+    let expense = 0;
+    let income = 0;
+    //check if the category is expense or income, if it is expense, let's the expense
+    transacts.map((payload) => {
+      payload.category === "Expense"
+        ? (expense = expense + payload.amount)
+        : (income = income + payload.amount);
+    });
+    setExpenses(expense);
+    setIncomes(income);
+  };
+
+  useEffect(() => {
+    calculateBalance();
+  }, [transacts]);
 
   const addTransactions = (payload) => {
     const transactsArray = [...transacts];
@@ -23,7 +44,11 @@ const Expense = () => {
 
   return (
     <Wrapper>
-      <TransactionOverview addTransactions={addTransactions} />
+      <TransactionOverview
+        addTransactions={addTransactions}
+        expsenses={expsenses}
+        incomes={incomes}
+      />
       <Transaction transacts={transacts} />
     </Wrapper>
   );
